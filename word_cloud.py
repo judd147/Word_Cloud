3c9b968add6de05bde7e71c45d9b3cf5cc1c3825
judd147 <57974800+judd147@users.noreply.github.com>
2022-06-08 18:25:14 +0800

    Update word_cloud.py

--- word_cloud.py
+++ word_cloud.py
@@ -18,6 +18,7 @@ def main():
 def jieba_cut():
     st.header("词云分析")
     st.caption('上传文本在线生成词云图')
+    num_color = st.number_input("字体颜色数量", min_value=3, max_value=8, value=3)
     with st.form(key='word_freq'):
         #文件设置
         fr = st.file_uploader("上传停用词", type='txt', key='word_cloud')
@@ -27,17 +28,36 @@ def jieba_cut():
         col1, col2, col3 = st.columns(3)
         with col1:
             num_show = st.number_input("输出词汇数（按排名）", min_value=1, max_value=100, value=100, help="显示词频表中前xxx个词")
-            color1 = st.color_picker('字体颜色1', '#228B22', help="自定义颜色")
             min_font_size = st.number_input("字体最小值", min_value=1, max_value=50, value=5)
             bg_color = st.color_picker('背景颜色', '#FFFFFF', help="词云图背景颜色，默认为白色")
+            
         with col2:
             max_words = st.number_input("显示最大词数", min_value=1, max_value=100, value=80, help="词云图显示最大词数")
-            color2 = st.color_picker('字体颜色2', '#CD661D', help="自定义颜色")
             max_font_size = st.number_input("字体最大值", min_value=50, max_value=300, value=150)
+            
+            
         with col3:
             colloc_choice = st.selectbox("允许分词重复", options=['否','是'])
-            color3 = st.color_picker('字体颜色3', '#5CACEE', help="自定义颜色")
             prefer_horizontal = st.number_input("词汇横排比例", min_value=0.0, max_value=1.0, value=1.0, help="区间为0-1，越趋于0竖向分词比例越高")
+        
+        col01, col02, col03, col04 = st.columns(4)
+        with col01:
+            color1 = st.color_picker('字体颜色', '#228B22', help="自定义颜色")
+        with col02:
+            color2 = st.color_picker('字体颜色', '#CD661D', help="自定义颜色")
+        with col03:
+            color3 = st.color_picker('字体颜色', '#5CACEE', help="自定义颜色")
+        color_list_input = [color1,color2,color3]
+        if num_color >= 4:
+            with col04:
+                color4 = st.color_picker('字体颜色', '#FFFFFF', help="自定义颜色")
+                color_list_input.append(color4)
+        if num_color >= 5:
+            col_list = st.columns(4)
+            for i in range(num_color-4):
+                with col_list[i]:
+                    color = st.color_picker('字体颜色', '#FFFFFF', help="自定义颜色", key=i)
+                    color_list_input.append(color)
 
         run = st.form_submit_button(label='运行')
         
@@ -76,11 +96,11 @@ def jieba_cut():
         st.download_button(
              label="下载词频表并退出",
              data=csv,
-             file_name='词频表.csv',
-             mime='csv',
+             file_name='词频表.txt',
+             mime='text',
         )
 
-        color_list=[color1,color2,color3]   #词云图的词颜色列表，设定想要呈现的颜色，几种均可.参考网址：https://tool.oschina.net/commons?type=3
+        color_list=color_list_input   #词云图的词颜色列表，设定想要呈现的颜色，几种均可.参考网址：https://tool.oschina.net/commons?type=3
         if colloc_choice == '是':
             collocations = True
         elif colloc_choice == '否':
@@ -89,7 +109,7 @@ def jieba_cut():
         wc = WordCloud(
                 background_color=bg_color,                # 词云图背景颜色，除了常见几种，还可使用background_color=WordCloud(background_color=(135,206,250)设定
                 max_words=max_words,                      # 词云图显示最大词数
-                font_path="SIMHEI.TTF",  # 使用字体 SIMHEI.TTF
+                font_path="/usr/share/fonts/simhei.ttf",  # 使用字体 SIMHEI.TTF
                 min_font_size=min_font_size,              # 字体最小尺寸
                 max_font_size=max_font_size,              # 字体最大尺寸
                 colormap=colors.ListedColormap(color_list),
@@ -103,7 +123,7 @@ def jieba_cut():
 
 @st.cache()
 def convert_df(df):
-    return df.to_csv(index=False).encode('UTF-8')
+    return df.to__string(index=False).encode('UTF-8')
 
 if __name__ == "__main__":
     main()
